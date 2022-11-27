4c3ff8ba0d0e9bf7c0355450d692367271bc922c
VladimirMinic <vladimirminicket@gmail.com>
2022-11-27 16:51:26 +0100

    add refactor test package and gitignore file

--- src/test/java/com/test/project/TC003_GET_Request.java
+++ src/test/java/com/test/project/TC003_GET_Request.java
@@ -5,9 +5,11 @@ import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TC003_GET_Request {
 
+    @Test
     public void getUserSingle(){
 
         //base URI
